9c1e0d6b7fcde110722ecf7baa9d3e8c7b91fd0f
Kelly Kilma de Sousa <kellykilmadesousa@gmail.com>
2022-08-02 20:38:52 -0300

    revisão da primeira atividade

--- SPRING/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ SPRING/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -19,44 +19,52 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/postagens") 
-@CrossOrigin(origins = "*", allowedHeaders = "*") 
+
+//indica para o spring que a classe é do tipo controladora rest, que a classe irá ser acionada a partir da URL(começa a atender endpoints), 
+//e RequestBody objeto que contém os dados que serão criados ou atualizados
+@RestController 
+@RequestMapping("/postagens") //mapear o caminho para acessar o ponto de acesso pela API Postman
+@CrossOrigin(origins = "*", allowedHeaders = "*") //para o frontend consumir esse API de qualquer linguagem que seja o front;
+//e para liberar o backend para uma aplicação externa em outra porta 
+
 public class PostagemController {
 
 	
-	@Autowired 
-	private PostagemRepository postagemRepository;
+	@Autowired //como não conseguimos instanciar uma interface, precisamos utilizar para encarrregar o spring de instanciar tudo o que precisa ser carregado
+	private PostagemRepository repository;
 	
-	@GetMapping
+	@GetMapping //sempre que vir uma requisição externa, de alguem que consuma essa API atraves de
+	//postagens e for um metodo GET vai disparar esse metodo
 	public ResponseEntity<List<PostagemModel>> getAll (){
-		return ResponseEntity.ok(postagemRepository.findAll());
+		return ResponseEntity.ok(repository.findAll());
+	//ResponseEntity<> é uma interface que monta um template no protocolo HTTP passando na body lista de produtos em formato Json
+	//findAll é implementado pela JPA e usado para carregar todos os dados na tabela 
 	}
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<PostagemModel> getById(@PathVariable Long id) {
-		return postagemRepository.findById(id)
-			.map(resposta -> ResponseEntity.ok(resposta))
+	@GetMapping("/{id}") //findbyid retorna apenas um item da nossa tabela
+	public ResponseEntity<PostagemModel> getById(@PathVariable Long id) { //pathvariable é uma forma de passar valores para a nossa API
+		return repository.findById(id) 
+			.map(resposta -> ResponseEntity.ok(resposta)) //metodo map() com empressao lambda expression para resposta positiva e para informar que caso tenha algum nulo o response sera um 404 not found 
 			.orElse(ResponseEntity.notFound().build());
 	}
 	
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<PostagemModel>> getByTitulo(@PathVariable String titulo){
-		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
 	@PostMapping
 	public ResponseEntity<PostagemModel> postPostagem (@Valid @RequestBody PostagemModel postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
 	}
 	
 	@PutMapping
 	public ResponseEntity<PostagemModel> putPostagem (@RequestBody PostagemModel postagem){
-		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 	}
 	
 	@DeleteMapping("/{id}")
 	public void deletePostagem(@PathVariable Long id) {
-		postagemRepository.deleteById(id);
+		repository.deleteById(id);
 	}
 }
--- SPRING/blogPessoal/src/main/java/org/generation/blogPessoal/model/PostagemModel.java
+++ SPRING/blogPessoal/src/main/java/org/generation/blogPessoal/model/PostagemModel.java
@@ -13,20 +13,22 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Entity //indica que a classe é uma entidade, será utilizada para gerar uma tabela no BD.
-@Table (name = "tb_postagens") // indica o nome da tabela no BD. Caso ela não seja declarada, o Banco criará a tabela com o mesmo nome da classe.
+@Entity //essa anotação indica que a classe é uma entidade, será utilizada para gerar uma tabela no BD.
+@Table (name = "tb_postagens") //indica o nome da tabela no BD. Caso ela não seja declarada, o banco criará a tabela com o mesmo nome da classe.
 public class PostagemModel {
 
-	@Id //indica o campo como uma chave primaria no SQL
-	@GeneratedValue(strategy = GenerationType.IDENTITY) //indica o esquema autoincrement para a chave primarianque será criada pelo BD
-	private Long id; //Long com L maiúsculo é uma classe wrapper, ela permite que a gente use aqueles getters e setter do java 
-	//o long com l minúsculo é uma classe primitiva, também permite os getters e setters mas pode dar conflito de tipagem
+	@Id //indica id como uma primary key no BD
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //indica o esquema autoincrement para a chave primaria que será criada pelo BD
+	private Long id; 
+	//Long com L maiúsculo é uma classe wrapper, permite que a gente use aqueles getters e setter do java 
+	//já o long com l minúsculo é uma classe primitiva, também permite os getters e setters mas pode dar conflito de tipagem
 
+	//serve para validar se valor inserido não está vazio, nulo e se tem espaço (geralmente para textos menores)
 	@NotBlank(message = "O atributo título é Obrigatório e não pode utilizar espaços em branco!") 
-	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres") //quantidade de caracter 
 	private String titulo;
 	
-	@NotNull(message = "O atributo texto é Obrigatório!") 
+	@NotNull(message = "O atributo texto é Obrigatório!") //serve para valida se está nulo e se tem espaço, geralmente textos maiores 
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
 	private String texto;
 	
