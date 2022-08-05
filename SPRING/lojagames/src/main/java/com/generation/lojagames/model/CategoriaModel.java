0cea1717a52663c9285a42700680a7c963bfc413
Kelly Kilma de Sousa <kellykilmadesousa@gmail.com>
2022-08-05 15:07:01 -0300

    05082022

---
+++ SPRING/lojagames/src/main/java/com/generation/lojagames/model/CategoriaModel.java
@@ -0,0 +1,59 @@
+package com.generation.lojagames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+	@Entity
+	@Table(name="tb_categoria")
+	public class CategoriaModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="o nome da categoria é obrigatório!")
+	@Size(min=5, max=100, message = "a categoria deve conter de 5 a 100 caracteres")
+	private String nome;
+	
+	@OneToMany(mappedBy ="categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<ProdutoModel> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<ProdutoModel> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<ProdutoModel> produto) {
+		this.produto = produto;
+	}
+
+	
+	
+}
---
+++ SPRING/lojagames/src/main/java/com/generation/lojagames/model/ProdutoModel.java
@@ -0,0 +1,79 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+@Entity
+@Table (name="tb_produtos")
+public class ProdutoModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="o nome do produto é obrigatório!")
+	@Size(min=5, max=100, message= "o nome do produto deve conter de 5 a 100 caracteres")
+	private String nome;
+	
+	@NotBlank(message="a decrição é obrigatório!")
+	@Size(min=10, max=500, message= "a descrição deve conter de 10 a 500 caracteres")
+	private String descricao;
+	
+	@Positive(message="o preço é obrigatório!")
+	private BigDecimal preco; 
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private CategoriaModel categoria;
+ 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public CategoriaModel getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaModel categoria) {
+		this.categoria = categoria;
+	}
+	
+}
